aea61cb981d5d2ed2e16fed524a5a33322c936ff
Terry-Rossi <terryrossi1@gmail.com>
2023-03-07 14:38:46 -0800

    Initial Commit for the BAP project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-alert("Hello World!");
+const bullsList = [
+  { id: 1, guild: "East", type: "Bull" },
+  { id: 2, guild: "South", type: "Bull" },
+  { id: 3, guild: "North", type: "God" },
+  { id: 4, guild: "West", type: "God" },
+];
+const labelList = [
+  "First",
+  "Second",
+  "third",
+  "Fourth",
+  "Fith",
+  "Sixth",
+  "Seventh",
+  "Eighth",
+  "Nineth",
+  "Tenth",
+];
 
-const favoriteFood = "...Paella!!!!";
-document.write(favoriteFood);
+for (i = 0; i < bullsList.length; i++) {
+  console.log(
+    `The ${labelList[i]} ${bullsList[i].type} is from the ${bullsList[i].guild} Guild`
+  );
+}
