1e1e0f3659355e07692342fa60c48d38bf49b4ad
Terry-Rossi <terryrossi1@gmail.com>
2023-03-11 12:13:02 -0800

    Mods for 1.5
    Added: forEach, IIFE, Check Object type, Check Object keys, added Search Function

--- js/script.js
+++ js/script.js
@@ -2,188 +2,159 @@
 //
 // Initialize list of Pokemons using IIFE to protect variables
 
-let pokemonRepository = (function () {
-    const pokemonList = [];
+let pokemon = {};
+const pokemonRepository = (function () {
+	const pokemonList = [];
 
-    let pokemon = {
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["grass", "poison"],
-        color: "green",
-    };
-    pokemonList.push(pokemon);
+	pokemon = {
+		name: "Bulbasaur",
+		height: 0.7,
+		types: ["grass", "poison"],
+		color: "green",
+	};
+	pokemonList.push(pokemon);
 
-    pokemon = {
-        name: "Ivysaur",
-        height: 1,
-        types: ["grass", "poison"],
-        color: "green",
-    };
-    pokemonList.push(pokemon);
+	pokemon = {
+		name: "Ivysaur",
+		height: 1,
+		types: ["grass", "poison"],
+		color: "green",
+	};
+	pokemonList.push(pokemon);
 
-    pokemon = {
-        name: "Venusaur",
-        height: 2,
-        types: ["grass", "poison"],
-        color: "green",
-    };
-    pokemonList.push(pokemon);
+	pokemon = {
+		name: "Venusaur",
+		height: 2,
+		types: ["grass", "poison"],
+		color: "green",
+	};
+	pokemonList.push(pokemon);
 
-    pokemon = {
-        name: "Charmander",
-        height: 0.6,
-        types: ["fire"],
-        color: "red",
-    };
-    pokemonList.push(pokemon);
+	pokemon = {
+		name: "Charmander",
+		height: 0.6,
+		types: ["fire"],
+		color: "red",
+	};
+	pokemonList.push(pokemon);
 
-    //
-    //
-    // Function add to add new Pokemons to the list
+	///////////////////////////////////  WE ARE STILL IN THE IIFE  /////////////////////////////////
+	//
+	// Function add to add new Pokemons to the list
 
-    function add(pokemon) {
-        // Check if argument received is NOT of type "Object"
-        if (typeof pokemon !== "object") {
-            console.log(
-                `The argument passed to the function "add" is of type ${typeof pokemon}`
-            );
-            console.log(
-                `The argument passed to the function "add" MUST be an object 
+	function add(pokemon) {
+		// Check if argument received is NOT of type "Object"
+		if (typeof pokemon !== "object") {
+			console.log(`The argument passed to the function "add" is of type ${typeof pokemon}`);
+			console.log(
+				`The argument passed to the function "add" MUST be an object 
         with the following keys: ${Object.keys(pokemonRepository.getAll()[0])}.`
-            );
-            console.log(
-                `The New Object has NOT been added to the list. Please correct the Error and Resubmit...`
-            );
-        } else {
-            // Pokemon is indeed an Object, now let's check if keys are the correct ones
+			);
+			console.log(`The New Object has NOT been added to the list. Please correct the Error and Resubmit...`);
+		} else {
+			// Pokemon is indeed an Object, now let's check if keys are the correct ones...
 
-            // Grab the Original list of keys of the pokemon object from the First element of the pokemonRepository
-            // for furure validation
-            const validPokemonKeys = Object.keys(pokemonRepository.getAll()[0]);
+			// Grab the Original list of keys of the pokemon object from the First element of the pokemonRepository
+			// for furure validation
+			const validPokemonKeys = Object.keys(pokemonRepository.getAll()[0]);
 
-            // Compare keys of argument received with keys of First element of the pokemonRepository
-            if (
-                Object.keys(pokemon).toString() !== validPokemonKeys.toString()
-            ) {
-                console.log(`INVALID KEYS of the received Argument: `);
-                console.log(`${Object.keys(pokemon)} `);
+			// Compare keys of argument received with valid keys
+			if (Object.keys(pokemon).toString() !== validPokemonKeys.toString()) {
+				console.log(`INVALID KEYS of the received Argument: `);
+				console.log(`${Object.keys(pokemon)} `);
 
-                console.log(`VALID KEYS are: `);
-                console.log(`${Object.keys(pokemonRepository.getAll()[0])}.`);
-                console.log(
-                    `The New Object has NOT been added to the list. Please correct the Error and Resubmit...`
-                );
-            } else {
-                // All is good, Push pokemon to the pokemonRepository
-                pokemonList.push(pokemon);
-            }
-        }
-    }
-    //
-    //
-    // Function getAll to return the list of all Pokemons
-    function getAll() {
-        return pokemonList;
-    }
-
-    return {
-        add,
-        getAll,
-    };
+				console.log(`VALID KEYS are: `);
+				console.log(`${Object.keys(pokemonRepository.getAll()[0])}.`);
+				console.log(`The New Object has NOT been added to the list. Please correct the Error and Resubmit...`);
+			} else {
+				// All is good, Push pokemon to the pokemonRepository
+				pokemonList.push(pokemon);
+			}
+		}
+	}
+	// end of function add
+	//
+	///////////////////////////////////  WE ARE STILL IN THE IIFE  /////////////////////////////////
+	//
+	// Function getAll to return the list of all Pokemons
+	//
+	function getAll() {
+		return pokemonList;
+	}
+	//
+	// end of function getAll
+	//
+	return {
+		add,
+		getAll,
+	};
 })();
-// END OF IIFE POKEMON INITIALIZATION FUNCTIONS
 //
-////////////////////////////////////////////////////////
+///////////////////////////////////   END OF IIFE POKEMON INITIALIZATION FUNCTIONS  /////////////////////////////////
 //
-//  HTML CONTAINERS
+//
+// DISPLAY POKEMONS FUNCTION. SENDS HTML TO THE DOM ...
+//
+function showHTML(pokemonList) {
+	pokemonList.forEach(function (pokemon) {
+		//   Test for Pokemon size over 1.5
+		const wow = pokemon.height >= 1.5 ? "<a class='wow'> (WOW!!! That is Huge!!!) </a>" : "";
 
-const form = document.getElementById("searchform");
-const pokemonSearchName = document.getElementById("search-pokemon").value;
-const containerPokemons = document.getElementById("containerPokemons");
+		// Format text when multiple types are present
+		const typeFormated = pokemon.types.join(" and ");
 
-//
-//
-///////////////////////////////////////////////////////
+		// Format the final text with Pokemon details. Should look like the example below:
+		// Bulbasaur is 0.7 meter tall and is of grass and poison types
+		// Each line should be of the color specified in the object.color
 
-//
-// DISPLAY POKEMONS FUNCTION...
-//
-function showHTML(pokemons) {
-    // Using ForEach Loop for each Pokemon
-    console.log(pokemons);
-    pokemons.forEach(function (pokemon) {
-        //   Ternary operator to fill up extra WOW! text for size over 1.5
-        let wow =
-            pokemon.height >= 1.5
-                ? "<a class='wow'> (WOW!!! That is Huge!!!) </a>"
-                : "";
-
-        // Format the pokemon types
-        let typeFormated = pokemon.types.join(" and ");
-
-        // Format the final text
-        const finalText = `<div class="${pokemon.color} ><p>
-  ${pokemon.name} is ${
-            pokemon.height
-        } meter tall ${wow}and is of ${typeFormated} ${
-            pokemon.types.length > 1 ? "types" : "type"
-        }
-</p></div>`;
-        console.log("finalText...");
-        console.log(finalText);
-        containerPokemons.insertAdjacentHTML("afterbegin", finalText);
-        containerPokemons.insertAdjacentText("afterbegin", "kjgjkgkjgiukjgkjg");
-    });
+		const finalText = `<div class="${pokemon.color}"><p>${pokemon.name} is ${pokemon.height} meter tall ${wow}
+    and is of ${typeFormated} ${pokemon.types.length > 1 ? "types" : "type"}</p></div>`;
+		//
+		document.write(finalText);
+	});
 }
 // END OF DISPLAY FUNCTION...
 //
-//
+////////////////////////////////////////////////////////////
 //
 // SEARCH FUNCTION...
 //
-function searchPokemon() {
-    // form.addEventListener("submit", (e) => {
-    //     e.preventDefault();
-    //     // Find Pokemon
-    //     console.log(`Search name: ${pokemonSearchName}`);
-    //     const pokemon = pokemonRepository.getAll().filter(function (pokemon) {
-    //         if (pokemon.name === pokemonSearchName) {
-    //             return pokemon;
-    //         }
-    //     });
-    //     console.log(`Pokemon searched:`);
-    //     console.log(pokemon);
-    //     containerPokemon.innerHTML = "";
-    //     showHTML(pokemon);
-    // });
+function searchPokemon(pokemonSearchName) {
+	// Find Pokemon
+	const pokemon = pokemonRepository.getAll().filter(function (pokemon) {
+		if (pokemon.name.toLowerCase() === pokemonSearchName.toLowerCase()) {
+			return pokemon;
+		}
+	});
+
+	showHTML(pokemon);
 }
+
 // END OF SEARCH FUNCTION...
 //
+////////////////////////////////////////////////////////////////////
 //
-//
-//
-// ************  PROGRAM FLOW... **************
+// ************  PROGRAM EXECUTION... **************
 //
 // Add a new Pokemon
 pokemon = {
-    name: "Titi",
-    height: 100,
-    types: ["car", "truck"],
-    color: "purple",
+	name: "Titi",
+	height: 100,
+	types: ["car", "truck"],
+	color: "purple",
 };
 pokemonRepository.add(pokemon);
 //
 //  SHOW ALL POKEMONS
 showHTML(pokemonRepository.getAll());
+
+// SHOW ONLY SEARCHED POKEMON
+// const pokemonSearchName = "VenUsaur";
+// searchPokemon(pokemonSearchName);
+
 //
 //
 // TESTING...
-
-// For testing: Function to send to the console list of all Pokemons
-// const showAllPokemons = function () {
-//     console.log(pokemonRepository.getAll());
-// };
-// showAllPokemons();
 
 // Add an invalid Pokemon (STRING) - TESTED!
 // pokemonRepository.add("titi");
@@ -212,13 +183,6 @@ showHTML(pokemonRepository.getAll());
 //     types: ["car", "truck"],
 //     color: "purple",
 // };
-
 // Add the new Pokemon
 // pokemonRepository.add(pokemon);
 // showAllPokemons();
-
-// CREATE HTML with Pokemon details. Should look like the example below:
-
-// Bulbasaur is 0.7 meter tall and is of grass and poison types
-
-// Each line should be of the color specified in the object
