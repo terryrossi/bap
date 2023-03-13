44f6caa4323f54197ebcc9b3e531f4d79b6a3e3f
Terry-Rossi <terryrossi1@gmail.com>
2023-03-13 12:11:03 -0700

    Update script.js
    Clean-up

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //
 // Initialize list of Pokemons using IIFE to protect variables
 
-// *******   IIFE   ********
+// DATA...
 const pokemonRepository = (function () {
 	const pokemonList = [];
 	let pokemon = {};
@@ -39,10 +39,7 @@ const pokemonRepository = (function () {
 	};
 	pokemonList.push(pokemon);
 
-	///////////////////////////////////  WE ARE STILL IN THE IIFE  /////////////////////////////////
-	//
-	// Function add to add new Pokemons to the list
-
+	// FUNCTIONS...
 	function add(pokemon) {
 		// Check if argument received is NOT of type "Object"
 		if (typeof pokemon !== "object") {
@@ -55,8 +52,6 @@ const pokemonRepository = (function () {
 				`The New Object has NOT been added to the list. Please correct the Error and Resubmit...`
 			);
 		} else {
-			// Pokemon is indeed an Object, now let's check if keys are the correct ones...
-
 			// find Valid keys from first ogject
 			const validPokemonKeys = Object.keys(pokemonRepository.getAll()[0]);
 
@@ -76,37 +71,12 @@ const pokemonRepository = (function () {
 			}
 		}
 	}
-	// end of function add
-	//
-	///////////////////////////////////  WE ARE STILL IN THE IIFE  /////////////////////////////////
-	//
-	// Function getAll to return the list of all Pokemons
-	//
+
 	function getAll() {
 		return pokemonList;
 	}
-	//
-	// end of function getAll
-	//
-	///////////////////////////////////////////////////
-	//
-	// function addListItem - Created the HTML for each Pokemon
 
 	function addListItem(pokemon) {
-		//   Test for Pokemon size over 1.5
-		const wow = pokemon.height >= 1.5 ? "<a class='wow'> (WOW!!! That is Huge!!!) </a>" : "";
-
-		// Format text when multiple types are present
-		const typeFormated = pokemon.types.join(" and ");
-
-		// Format the final text with Pokemon details.
-		const finalText = `<div class="${pokemon.color}"><p>${pokemon.name} is ${
-			pokemon.height
-		} meter tall ${wow}
-    and is of ${typeFormated} ${pokemon.types.length > 1 ? "types" : "type"}</p></div>`;
-		//
-		// Format HTML List Item;
-		//
 		// Variables from HTML Elements:
 		const pokemonHtmlList = document.querySelector(".pokemon-list");
 		const li = document.createElement("li");
@@ -119,9 +89,6 @@ const pokemonRepository = (function () {
 		button.innerText = pokemon.name;
 
 		// add eventListener to the Button
-		// button.addEventListener("click", function (event) {
-		// 	showDetails(pokemon);
-		// });
 		addEventListenerToButton(button, "click", pokemon);
 
 		// insert the button into the line element
@@ -133,32 +100,17 @@ const pokemonRepository = (function () {
 		// add the line element into the list element
 		pokemonHtmlList.appendChild(li);
 	}
-	//  End of addListItem
-	//
-	/////////////////////////////////////////////////////////////////
-	//
-	// Add an Event Listener to a Button
-	//
+
 	function addEventListenerToButton(button, type, pokemon) {
 		button.addEventListener(type, function (event) {
 			showDetails(pokemon);
 		});
 	}
-	// end of Add an Event Listener to a Button
-	//
-	//////////////////////////////////////////////////////////////////
-	//
-	// SHOW DETAILS FUNCTION...
-	//
+
 	function showDetails(pokemon) {
 		console.log(pokemon);
 	}
-	//  End of show details function
-	//
-	//////////////////////////////////////////////////////////////////
-	//
-	// SEARCH FUNCTION...
-	//
+
 	function search(searchName) {
 		// Find Pokemon
 		const foundPokemon = pokemonRepository.getAll().filter(function (pokemon) {
@@ -168,23 +120,13 @@ const pokemonRepository = (function () {
 		});
 		return foundPokemon;
 	}
-	//
-	// END OF SEARCH FUNCTION...
-	//
-	////////////////////////////////////////////////////////////////////
-	//
-	// DISPLAY POKEMONS FUNCTION. SENDS HTML TO THE DOM ...
-	//
+
 	function showHTML(pokemonList) {
 		pokemonList.forEach(function (pokemon) {
 			pokemonRepository.addListItem(pokemon);
 		});
 	}
-	// END OF DISPLAY FUNCTION...
-	//
-	////////////////////////////////////////////////////////////
 
-	// Returning all functions from IIFE
 	return {
 		add,
 		getAll,
@@ -202,7 +144,7 @@ const pokemonRepository = (function () {
 //
 // Add a new Pokemon
 const newPokemon = {
-	name: "Titi",
+	name: "Tutu",
 	height: 100,
 	types: ["car", "truck"],
 	color: "purple",
@@ -216,37 +158,3 @@ pokemonRepository.showHTML(pokemonRepository.getAll());
 // const pokemonSearchName = "VenUsaur";
 // const resultOfSearch = pokemonRepository.search(pokemonSearchName);
 // pokemonRepository.showHTML(resultOfSearch);
-//
-//
-// TESTING...
-
-// Add an invalid Pokemon (STRING) - TESTED!
-// pokemonRepository.add("titi");
-
-// Add a new Pokemon (INVALID KEY "name") and show the new list on Console - TESTED!
-// pokemon = {
-//   names: "toto",
-//   height: 100,
-//   types: ["car", "truck"],
-//   color: "purple",
-// };
-
-// Add a new Pokemon (INVALID extra KEY) and show the new list on Console - TESTED!
-// pokemon = {
-//   name: "toto",
-//   height: 100,
-//   types: ["car", "truck"],
-//   color: "purple",
-//   age: 27,
-// };
-
-// Add a new Pokemon (VALID) and show the new list on Console - TESTED!
-// pokemon = {
-//     name: "Toto",
-//     height: 100,
-//     types: ["car", "truck"],
-//     color: "purple",
-// };
-// Add the new Pokemon
-// pokemonRepository.add(pokemon);
-// showAllPokemons();
